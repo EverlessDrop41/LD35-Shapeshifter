f90826bcda1f1aa794ff0bbf12eeb2c8187433e3
EverlessDrop41 <tomfperegrine@googlemail.com>
2016-04-16 18:58:17 +0100

    Added Main Menu and altered controls

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public void LoadScene(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+}
