381b437423e48354629678afe5ee93498af559e6
EverlessDrop41 <tomfperegrine@googlemail.com>
2016-04-17 02:46:20 +0100

    Completed level3

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioClip GameLoop;
+    private AudioSource _source;
+
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+
+        if (Instance == null)
+        {
+            Instance = this;
+            _source.loop = true;
+            _source.clip = GameLoop;
+            if (!_source.isPlaying) { _source.Play();}
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            _source.Stop();
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+}
---
+++ Assets/Scripts/Unitricity/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger: ElectricalObject
+{
+    public ElectricalObject Input;
+
+    public override bool IsOutputting()
+    {
+        return _hasBeenTurnedOn;
+    }
+
+    private bool _hasBeenTurnedOn = false;
+
+    void Update () {
+        if (!_hasBeenTurnedOn)
+        {
+            if (Input.IsOutputting())
+            {
+                _hasBeenTurnedOn = true;
+            }
+        }
+	}
+}
