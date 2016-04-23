e8c9e1420489a5df5acf20680eed627643ad159a
emilyperegrine <peregrineemily@gmail.com>
2016-04-23 16:01:22 +0100

    Added move to open position on door editor

---
+++ Assets/Editor/DoorEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(DoorControl))]
+public class DoorEditor : Editor{
+	public override void OnInspectorGUI ()
+	{
+		DoorControl door = (DoorControl)target;
+
+		DrawDefaultInspector ();
+
+		EditorGUILayout.HelpBox (
+			"The red cuboid shows the closed door position and the green cuboid shows the open position", 
+			MessageType.Info
+		);
+
+		if (GUILayout.Button("Move to start position")) {
+			if (door.OpenOnStart) {
+				door.transform.position = door.OpenedPosition.position;
+			} else {
+				door.transform.position = door.ClosedPosition.position;
+			}
+		}
+	}
+}
